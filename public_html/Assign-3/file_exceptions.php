63bbe4483269da95bff5215c77e702251cb8b232
Warren Davis <55168816+Quarz8@users.noreply.github.com>
2019-09-29 16:58:43 -0400

    Assignment 3
    its public_html after doing assignment 3!

---
+++ public_html/Assign-3/file_exceptions.php
@@ -0,0 +1,34 @@
+<?php
+// idk how this works tbh, but i need it so processorders.php doesn't implode
+
+class fileOpenException extends Exception
+{
+function __toString()
+{
+return "fileOpenException ". $this->getCode()
+. ": ". $this->getMessage()."<br />"." in "
+. $this->getFile(). " on line ". $this->getLine()
+. "<br />";
+}
+}
+class fileWriteException extends Exception
+{
+function __toString()
+{
+return "fileWriteException ". $this->getCode()
+. ": ". $this->getMessage()."<br />"." in "
+. $this->getFile(). " on line ". $this->getLine()
+. "<br />";
+}
+}
+class fileLockException extends Exception
+{
+function __toString()
+{
+return "fileLockException ". $this->getCode()
+. ": ". $this->getMessage()."<br />"." in "
+. $this->getFile(). " on line ". $this->getLine()
+. "<br />";
+}
+}
+?>
